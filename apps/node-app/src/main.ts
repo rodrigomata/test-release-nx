0d08053120f74c1b9c4cb5703d642139d60eb98d
royestone <rodrigo.mata.ib@gmail.com>
2022-01-04 11:53:04 -0600

    test: test console

--- apps/node-app/src/main.ts
+++ apps/node-app/src/main.ts
@@ -8,7 +8,7 @@ import * as express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-  console.log('test 1')
+  console.log('test 2')
   res.send({ message: 'Welcome to node-app!' });
 });
 
